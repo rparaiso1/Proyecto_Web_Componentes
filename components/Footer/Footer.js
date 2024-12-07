300824468d9ec2b1262fe3d7fd9d4d7ef977d41d
rparaiso1 <rparaisomateo1@gmail.com>
2024-12-07 13:53:33 +0100

    Actualization
    Actualization

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const footer = document.createElement("footer");
   footer.innerHTML = `
     <footer class="footer">
-        <p>&copy; 2024 YourName. All Rights Reserved.</p>
+        <p>&copy; 2024 Ruben.P. All Rights Reserved.</p>
     </footer>
     `;
 
